6e0ee206f6be70e35e0bf5ce12aca1a83044fb1f
Vinicius Gomes <vinigomes.0311@gmail.com>
2024-05-27 23:57:39 -0300

    chore: Update tsconfig.json and package.json
    This commit updates the `tsconfig.json` file to remove the `tests` directory from the `include` property and removes the trailing comma in the `exclude` property.
    
    It also updates the `package.json` file to update the `eslint` package to version 8.57.0, add the `eslint-config-prettier` package, and add the `eslint-plugin-prettier` and `eslint-plugin-simple-import-sort` packages.
    
    These changes improve the configuration and dependencies of the project.

--- src/core/application/ports/ProductRepository.ts
+++ src/core/application/ports/ProductRepository.ts
@@ -1,7 +1,7 @@
 import { Product } from '../../domain/entities/Product'
 
 export interface IProductRepository {
-  addProduct(product: Product): Promise<void>
+  addProduct(product: Product): Promise<Product | void>
   editProduct(product: Product): Promise<void>
   removeProduct(productId: number): Promise<void>
   getProductsByIds(productIds: number[]): Promise<Product[]>
